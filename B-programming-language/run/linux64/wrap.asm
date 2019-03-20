e34f9134bec37bfbfad485644c001a7f45f2ee19
Pedro Rodrigues <pedro.p.rodrigues@tecnico.ulisboa.pt>
2019-03-20 14:48:17 +0000

    Changed ignored files

--- B-programming-language/run/linux64/wrap.asm
+++
@@ -1,9 +0,0 @@
-section .text
-	global _putstr, _putint, _getint, _getstr, _getchar
-	extern _prints, _printi, _readi, _readln, _readb
-
-_putstr:	jmp _prints
-_putint:	jmp _printi
-_getint:	jmp _readi
-_getstr:	jmp _readln
-_getchar:	jmp _readb
